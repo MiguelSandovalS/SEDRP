8a0e460661afe0e88230d579e3ca80c6b91498b1
MSS17 <miguelsandoval20470266@gmail.com>
2024-04-29 21:24:35 -0600

    Implementar el diseño basico de la pagina de inicio

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,10 +4,10 @@ import {getAuth, signOut} from 'firebase/auth'
 const auth = getAuth(appFirebase)
 function Home({correo}) {
   return (
-    <div className="">
-        <h1>pagina de inicio</h1>
+    <div className="flex flex-col items-center text-5xl font-blod text-center">
+        <h1>Pagina de inicio</h1>
         <h2>Bienvenido {correo}</h2>
-        <button onClick={() => signOut(auth)}>Salir</button>
+        <button onClick={() => signOut(auth)} className="w-20  active:scale-[.98] active:duration-75 hover:scale-[1.01] ease-in-out transition-all py-2 rounded-xl bg-sky-600 text-white text-lg font-bold mt-5">Salir</button>
     </div>
     
   )
